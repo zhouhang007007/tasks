35ef88728b72f9f79262b8128a3d9dc0935b52ca
CaesarWu21 <wujingze0721@gmail.com>
2018-05-25 15:48:06 +0800

    send NickName to broswer

--- A5_ChatRoom/Client.py
+++ A5_ChatRoom/Client.py
@@ -8,37 +8,38 @@ class Client(QMainWindow, mainwindow_ui.Ui_MainWindow):
     def __init__(self, host, port):
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.sendThreadFunc)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
         self.sock.connect((host, port))
         self.sock.send(b'1')
+        self.pushButton_2.clicked.connect(self.send)#22
 
     def sendThreadFunc(self):
-        try:
-            myword = self.lineEdit.text()
-            self.sock.send(myword.encode())
-            myword = "                                                     You: " + myword
-            self.textBrowser.append(myword)
-            self.textBrowser.update()
-            self.lineEdit.setText("")
-        except ConnectionAbortedError:
-            self.textBrowser.append('Server closed this connection!')
-            self.textBrowser.update()
-        except ConnectionResetError:
-            self.textBrowser.append('Server is closed!')
-            self.textBrowser.update()
+        while True:
+            try:
+                myword = input()
+                self.sock.send(myword.encode())
+            except ConnectionAbortedError:
+                print('Server closed this connection!')
+            except ConnectionResetError:
+                print('Server is closed!')
 
     def recvThreadFunc(self):
         while True:
             try:
                 otherword = self.sock.recv(1024) # socket.recv(recv_size)
-                self.textBrowser.append(otherword.decode())
-                self.textBrowser.update()
+                print(otherword.decode())
             except ConnectionAbortedError:
-                self.textBrowser.append('Server closed this connection!')
+                print('Server closed this connection!')
             except ConnectionResetError:
-                self.textBrowser.append('Server is closed!')
+                print('Server is closed!')
+
+
+    def send(self):#11
+        text = self.lineEdit_2.text()#222
+        self.textBrowser.append(text)#222
+        self.textBrowser.update()#222
+        self.lineEdit_2.setText("")#222
 
 def main():
     c = Client('localhost', 5550)
@@ -49,6 +50,8 @@ def main():
         t.setDaemon(True)
         t.start()
     t.join()
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
