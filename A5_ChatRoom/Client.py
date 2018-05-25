093fb105616f2d89295d07c48a10f5d6e44af689
cyrus1997thy <cyrus1997thy@gmail.com>
2018-05-25 15:21:58 +0800

    send

--- A5_ChatRoom/Client.py
+++ A5_ChatRoom/Client.py
@@ -8,30 +8,36 @@ class Client(QMainWindow, mainwindow_ui.Ui_MainWindow):
     def __init__(self, host, port):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.sendThreadFunc)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
         self.sock.connect((host, port))
         self.sock.send(b'1')
 
     def sendThreadFunc(self):
-        while True:
-            try:
-                myword = input()
-                self.sock.send(myword.encode())
-            except ConnectionAbortedError:
-                print('Server closed this connection!')
-            except ConnectionResetError:
-                print('Server is closed!')
+        try:
+            myword = self.lineEdit.text()
+            self.sock.send(myword.encode())
+            self.textBrowser.append(myword+" :You")
+            self.textBrowser.update()
+            self.lineEdit.setText("")
+        except ConnectionAbortedError:
+            self.textBrowser.append('Server closed this connection!')
+            self.textBrowser.update()
+        except ConnectionResetError:
+            self.textBrowser.append('Server is closed!')
+            self.textBrowser.update()
 
     def recvThreadFunc(self):
         while True:
             try:
                 otherword = self.sock.recv(1024) # socket.recv(recv_size)
-                print(otherword.decode())
+                self.textBrowser.append(otherword.decode())
+                self.textBrowser.update()
             except ConnectionAbortedError:
-                print('Server closed this connection!')
+                self.textBrowser.append('Server closed this connection!')
             except ConnectionResetError:
-                print('Server is closed!')
+                self.textBrowser.append('Server is closed!')
 
 def main():
     c = Client('localhost', 5550)
