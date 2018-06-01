0d65f4af111967ab3fd306bb6a51a97997ffc0e3
zhouhang007007 <32261177+zhouhang007007@users.noreply.github.com>
2018-06-01 15:20:48 +0800

    Add files via upload

--- A5_ChatRoom/Client.py
+++ A5_ChatRoom/Client.py
@@ -8,10 +8,12 @@ class Client(QMainWindow, mainwindow_clinet.Ui_MainWindow):
     def __init__(self, host, port):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+        self.PushButton_Send.clicked.connect(self.sendThreadFunc)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
         self.sock.connect((host, port))
         self.sock.send(b'1')
+        self.PushButton_Login.clicked.connect(self.send)#22
 
     def sendThreadFunc(self):
         try:
@@ -42,16 +44,27 @@ class Client(QMainWindow, mainwindow_clinet.Ui_MainWindow):
 
 
     def send(self):#11
-        name = self.lineEdit_2.text()#222
+        name = self.lineEdit_Name.text()#222
         self.sock.send(name.encode())
         self.textBrowser.update()#222
-        self.lineEdit_2.setText("")#222
+        self.lineEdit_Name.setText("")#222
         th2 = threading.Thread(target=self.recvThreadFunc)
         th2.setDaemon(True)
         th2.start()
+
+def main():
+    c = Client('localhost', 5550)
+    th1 = threading.Thread(target=c.sendThreadFunc)
+    th2 = threading.Thread(target=c.recvThreadFunc)
+    threads = [th1, th2]
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+    t.join()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     MainWindow = Client('localhost', 5550)
     MainWindow.show()
     sys.exit(app.exec_())
+    main()
